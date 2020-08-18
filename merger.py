709dd7c62954ef531fffdea78a3b7b1db98f5332
haloflooder <haloflooder@gmail.com>
2020-08-17 20:35:07 -0400

    Made it better for lazy people who doesn't want to rename folders and shit
    Added argument support for folder names.

--- merger.py
+++ merger.py
@@ -1,12 +1,40 @@
+import argparse
 import nbt
 import os
-import os.path
 import shutil
 from region import RegionFile
 
-for file in os.listdir('source'):
-	source_file_name = 'source/' + file
-	destination_file_name = 'destination/' + file
+def isValidWorldFolder(checkFolder):
+    if os.path.exists(checkFolder) == False or os.path.isfile(checkFolder):
+        return False
+    
+    regionFolder = checkFolder + '/region/'
+    if os.path.exists(regionFolder) and os.path.isfile(regionFolder) == False:
+        return True
+
+    return False
+
+aParser = argparse.ArgumentParser()
+
+aParser.add_argument('-s', '--source', required=True, help='Source world folder')
+aParser.add_argument('-d', '--dest', required=True, help='Destination world folder')
+
+args = aParser.parse_args()
+
+if isValidWorldFolder(args.source):
+    print(args.source + ' is not a valid world folder!')
+    exit()
+
+if isValidWorldFolder(args.dest):
+    print(args.dest + ' is not a valid world folder!')
+    exit()
+
+sourceRegionFolder = args.source + "/region/"
+destRegionFolder = args.dest + "/region/"
+
+for file in os.listdir(sourceRegionFolder):
+	source_file_name = sourceRegionFolder + file
+	destination_file_name = destRegionFolder + file
 	file_exists = os.path.isfile(destination_file_name)
 	if not file_exists:
 		shutil.copyfile(source_file_name, destination_file_name)
@@ -22,5 +50,5 @@ for file in os.listdir('source'):
 			if source_chunk is not None:
 				destination_chunk = destination.get_chunk(x, z)
 				if destination_chunk is None:
-					print("Write " + str(x) + "," + str(z))
+					print('Write ' + str(x) + ',' + str(z))
 					destination.write_chunk(x, z, source_chunk)
