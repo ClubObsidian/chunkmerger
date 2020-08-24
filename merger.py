67b359313ae6ac760401acbdb3b562b9c3436fda
haloflooder <haloflooder@gmail.com>
2020-08-24 08:25:09 -0400

    Prevents the tool from crashing while in use

--- merger.py
+++ merger.py
@@ -5,14 +5,14 @@ import shutil
 from region import RegionFile
 
 def is_valid_world_folder(check_folder):
-    if os.path.exists(check_folder) == False or os.path.isfile(check_folder):
-        return False
-    
-    region_folder = check_folder + '/region/'
-    if os.path.exists(region_folder) and os.path.isfile(region_folder) == False:
-        return True
+	if os.path.exists(check_folder) == False or os.path.isfile(check_folder):
+		return False
+	
+	region_folder = check_folder + '/region/'
+	if os.path.exists(region_folder) and os.path.isfile(region_folder) == False:
+		return True
 
-    return False
+	return False
 
 arg_parser = argparse.ArgumentParser()
 
@@ -22,15 +22,17 @@ arg_parser.add_argument('-d', '--destination', '--dest', dest='dest', required=T
 args = arg_parser.parse_args()
 
 if not is_valid_world_folder(args.source):
-    print(args.source + ' is not a valid world folder!')
-    exit()
+	print(args.source + ' is not a valid world folder!')
+	exit()
 
 if not is_valid_world_folder(args.dest):
-    print(args.dest + ' is not a valid world folder!')
-    exit()
+	print(args.dest + ' is not a valid world folder!')
+	exit()
 
 source_region_folder = args.source + "/region/"
 dest_region_folder = args.dest + "/region/"
+
+failed_regions = []
 
 for file in os.listdir(source_region_folder):
 	source_file_name = source_region_folder + file
@@ -41,14 +43,24 @@ for file in os.listdir(source_region_folder):
 		print('Destination ' + destination_file_name + ' did not exist, copying...')
 		continue
 
-	source = RegionFile(source_file_name)
-	destination = RegionFile(destination_file_name)
+	try:
+		print('Destination "' + destination_file_name + '" exists, merging...')
+		source = RegionFile(source_file_name)
+		destination = RegionFile(destination_file_name)
+		for x in range(0, 32):
+			for z in range(0, 32):
+				source_chunk = source.get_chunk(x, z)
+				if source_chunk is not None:
+					destination_chunk = destination.get_chunk(x, z)
+					if destination_chunk is None:
+						print('Write ' + str(x) + ',' + str(z))
+						destination.write_chunk(x, z, source_chunk)
+	except Exception as e:
+		print("Couldn't merge to " + destination_file_name)
+		print(e)
+		print('')
+		failed_regions.append(destination_file_name)
 
-	for x in range(0, 32):
-		for z in range(0, 32):
-			source_chunk = source.get_chunk(x, z)
-			if source_chunk is not None:
-				destination_chunk = destination.get_chunk(x, z)
-				if destination_chunk is None:
-					print('Write ' + str(x) + ',' + str(z))
-					destination.write_chunk(x, z, source_chunk)
+print('\nDone!\n')
+print('Regions that failed to merge:')
+print(*failed_regions, sep='\n')
